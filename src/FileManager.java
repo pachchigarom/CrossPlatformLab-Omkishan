8e408f260c7d0e23b8bb9d0d5d37d06aae2c531e
pachchigarom <pachchio@sheridancollege.ca>
2026-02-19 17:14:53 -0500

    Initital commit

---
+++ src/FileManager.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ompac
+ */
+public class FileManager {
+    
+    public void abc() {
+        System.out.println("Hello World!!");
+    }
+}
